cd028f99c04e1fdce1f3eb0b0b6f27b5a5597313
hkrds1996 <617980477@qq.com>
2021-06-25 17:20:59 -0700

    ......

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,43 +1,58 @@
 import React, { useEffect, useState } from "react";
 import ReactMarkdown from 'react-markdown'
 function Home() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-    const apiUrl = "https://"+process.env.REACT_APP_APIURL+"/articles";
-    useEffect(() => {
-        fetch(apiUrl)
-          .then(res => res.json())
-          .then(
-            (result) => {
-              setIsLoaded(true);
-              setItems(result);
-            },
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
-            (error) => {
-              setIsLoaded(true);
-              setError(error);
-            }
-          )
-      }, [])
-    
-      if (error) {
-        return <div>Error: {error.message}</div>;
-      } else if (!isLoaded) {
-        return <div>Loading...</div>;
-      } else {
-        return (
-          <div className="row  justify-content-center">
-            {items.map(item => (
-                <div className="blog col-lg-3 justify-content-center">
-                    <ReactMarkdown className="article" children ={item.content.substring(0,500)}></ReactMarkdown>
-                    <a href={"/#/articles/"+item._id}>Read More</a>                  
-                </div>                              
-            ))}
-          </div>
-        );
-      }
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const apiUrl = "https://" + process.env.REACT_APP_APIURL + "/articles";
+  useEffect(() => {
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <div className="row  justify-content-center">
+          {items.map(item => (
+            <div className="blog col-lg-3 justify-content-center">
+              <ReactMarkdown className="article" children={item.content.substring(0, 500)}></ReactMarkdown>
+              <a href={"/#/articles/" + item._id}>Read More</a>
+            </div>
+          ))}
+        </div>
+        <nav aria-label="Page navigation">
+          <ul class="pagination justify-content-center">
+            <li class="page-item disabled">
+              <a class="page-link" href="#" tabindex="-1">Previous</a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+              <a class="page-link" href="#">Next</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 export default Home;
